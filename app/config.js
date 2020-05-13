866f27c6d6afad0e02e6e0b4ba02abf60db9f9a9
Michael Gearon <mgearon@companieshouse.gov.uk>
2020-05-13 18:39:58 +0100

    Clean up

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Collect a Companies House document online',
+  serviceName: 'Collect a company document online',
 
   // Default port that prototype runs on
   port: '3000',
